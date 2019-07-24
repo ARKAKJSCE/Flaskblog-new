5ad0e445aa156b15f788379765e00272cf624c9f
ARKAKJSCE <arkaprava.p@somaiya.edu>
2019-07-25 00:26:49 +0530

    First commit

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -65,7 +65,17 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',image_file=image_file, form=form)
 
-    
+@users.route("/account1", methods=['GET', 'POST'])
+@login_required
+def account_delete():
+      #return 'Hello'
+      user = User.query.get_or_404(current_user)
+      user.deleted = True
+      db.session.delete(user)
+      db.session.commit()
+      flash('Your account has been deleted!', 'success')
+      return redirect(url_for('main.home'))
+
 
 @users.route("/user/<string:username>")
 def user_posts(username):
